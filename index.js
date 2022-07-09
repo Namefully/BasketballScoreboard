291b5f9383cf85f9f083b07d94c7680496158a2b
Namefully <108553244+Namefully@users.noreply.github.com>
2022-07-10 02:36:12 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+let homeEl = document.getElementById("homenum")
+let guestEl = document.getElementById("guest")
+let count = 0
+let count2 = 0
+function add1a(){
+ count += 1
+ homeEl.textContent = count
+}
+function add2a(){
+ count += 2
+ homeEl.textContent = count
+}
+function add3a(){
+ count += 3
+ homeEl.textContent = count
+}
+function add1b(){
+ count2 += 1
+ guestEl.textContent = count2
+}
+function add2b(){
+ count2 += 2
+ guestEl.textContent = count2
+}
+function add3b(){
+ count2 += 3
+ guestEl.textContent = count2
+}
